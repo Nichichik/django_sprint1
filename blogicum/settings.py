5f4f2d6d3b6da2ae7244ffa2b5e3b893714545d9
Nichichik <meltsova05@bk.ru>
2024-12-10 14:24:00 +0300

    Update settings.py

--- blogicum/settings.py
+++ blogicum/settings.py
@@ -56,7 +56,9 @@ ROOT_URLCONF = 'blogicum.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'html'  # Убедитесь, что путь корректен
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -68,6 +70,7 @@ TEMPLATES = [
         },
     },
 ]
+
 
 WSGI_APPLICATION = 'blogicum.wsgi.application'
 
